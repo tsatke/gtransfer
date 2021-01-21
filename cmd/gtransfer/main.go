f5b62ebdc27624a9bdfce16a2e6922c59efa2769
tsatke <tim.satke@gmail.com>
2021-01-21 08:05:01 +0100

    Add command line doc

--- cmd/gtransfer/main.go
+++ cmd/gtransfer/main.go
@@ -20,7 +20,16 @@ var (
 	}
 
 	serverCmd = &cobra.Command{
-		Use:  "serve",
+		Use:   "serve",
+		Short: "Serves a directory on any free port. Only arg is the directory.",
+		Long: `Serves the given directory on port :0, which will make the system choose a free port
+for you. The port is written to stdout in a formatted message. An example call would be
+the following.
+
+$ gtransfer serve /Users/jdoe/Desktop/localFolder
+
+This would serve the localFolder folder on a free port, available for a gtransfer client to download.
+See 'gtransfer help dial' for help on how to do that.`,
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			path := args[0]
@@ -38,7 +47,16 @@ var (
 	}
 
 	clientCmd = &cobra.Command{
-		Use:  "dial",
+		Use:   "dial",
+		Short: "Download the served directory. First arg is the remote address, second is the path to download to.",
+		Long: `Dials the remote address, which is the first argument. The server serves a certain directory,
+which is written under the target path on the local system. This target path is the second argument.
+An example call would be the following.
+
+$ gtransfer dial 10.10.10.10 /Users/jdoe/Desktop/remoteSystem
+
+This would download the served directory on the host into the remoteSystem folder.
+see 'gtransfer help serve' for help on how to setup a server.`,
 		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			addr := args[0]
